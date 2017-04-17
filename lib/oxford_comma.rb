58f99a26e288f28fe54582483930abc5ed1b04af
Luis del Cueto <luisdelcueto@live.com>
2017-04-17 13:42:19 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array = array.join
+  elsif array.size == 2
+    array = array.join(" and ")
+  elsif array.size >= 3
+    third_element = array.pop
+    third_element = "and " + third_element
+    array.push(third_element)
+    array = array.join(", ")
+  end
+  array
 end
